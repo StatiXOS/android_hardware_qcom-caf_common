a98f99d4e95ddc8395df87519a8bc9814fc5ed5c
Michael Bestas <mkbestas@lineageos.org>
2024-12-11 18:42:15 +0200

    Drop hardware/qcom/keymaster from os_pickup_aosp.mk
    Change-Id: Ie8e0ae05a6f775ca99611a2a062bca265fde820d

--- os_pickup_aosp.mk
+++ os_pickup_aosp.mk
@@ -3,8 +3,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
-include $(LOCAL_PATH)/keymaster/Android.mk
-else
+ifneq ($(PRODUCT_USES_QCOM_HARDWARE),true)
 include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
